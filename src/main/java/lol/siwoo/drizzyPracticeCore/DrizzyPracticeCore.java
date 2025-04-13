6de51c54cdef5e470a471bf33bf5c5e4112cb473
siwoo <jamieyun06@gmail.com>
2025-04-13 12:17:24 +0900

    Initial commit

---
+++ src/main/java/lol/siwoo/drizzyPracticeCore/DrizzyPracticeCore.java
@@ -0,0 +1,17 @@
+package lol.siwoo.drizzyPracticeCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DrizzyPracticeCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
