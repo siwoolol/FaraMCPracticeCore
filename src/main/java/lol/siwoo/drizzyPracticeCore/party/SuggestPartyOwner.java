e2732fd59ef0b1ca2a854c7bfee9b28c158e37fb
siwoo <jamieyun06@gmail.com>
2025-04-13 17:30:57 +0900

    SuggestPartyOwner

---
+++ src/main/java/lol/siwoo/drizzyPracticeCore/party/SuggestPartyOwner.java
@@ -0,0 +1,32 @@
+package lol.siwoo.drizzyPracticeCore.party;
+
+import ga.strikepractice.StrikePractice;
+import ga.strikepractice.api.StrikePracticeAPI;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static org.bukkit.Bukkit.getPlayer;
+
+public class SuggestPartyOwner implements CommandExecutor {
+
+    StrikePracticeAPI api = StrikePractice.getAPI();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        Player p = (Player) sender;
+
+        String ownerString = api.getParty(p).getOwner();
+        Player owner = getPlayer(ownerString);
+
+        owner.sendMessage(ChatColor.AQUA + p.getName() + " has Asked You to Hurry up!");
+        p.sendMessage(ChatColor.GREEN + "You have Asked The Owner of the Party to Hurry up!");
+        return false;
+    }
+
+    public void openGui(Player p) {
+        // e
+    }
+}
