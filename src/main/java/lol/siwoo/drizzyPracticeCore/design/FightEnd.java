3b2d516ae715f1a6265a0a71f9da59e304b7b773
siwoo <jamieyun06@gmail.com>
2025-04-13 13:08:47 +0900

    design elements

---
+++ src/main/java/lol/siwoo/drizzyPracticeCore/design/FightEnd.java
@@ -0,0 +1,57 @@
+package lol.siwoo.drizzyPracticeCore.design;
+
+import ga.strikepractice.events.DuelEndEvent;
+import ga.strikepractice.events.DuelStartEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FightEnd extends JavaPlugin implements Listener {
+    private final List<String> defeatMessages = new ArrayList<>();
+
+    public void randomDefeatMessage() {
+        defeatMessages.add(" Has Obliterated ");
+        defeatMessages.add(" Has Touched Up Against ");
+        defeatMessages.add(" Has Defeated ");
+        defeatMessages.add(" Destroyed ");
+        defeatMessages.add(" Has Won Against ");
+    }
+
+    @EventHandler
+    public void onFightEnd(DuelEndEvent event) {
+        randomDefeatMessage();
+        Player w = event.getWinner();
+        Player l = event.getLoser();
+
+        // Shuffle Random Messages
+        List<String> shuffled = new ArrayList<>(defeatMessages);
+        Collections.shuffle(shuffled);
+        String selectedMessage = shuffled.get(0);
+
+        // Winner Prompt
+        w.playSound((w.getLocation()), Sound.EXPLODE, 1, 1);
+        l.sendTitle(ChatColor.GREEN + "VICTORY", ChatColor.GREEN + w.getName() + ChatColor.WHITE + selectedMessage + ChatColor.GREEN + l.getName());
+        w.sendMessage(ChatColor.GREEN + "You Won the Match");
+
+        // Loser Prompt
+        l.playSound((l.getLocation()), Sound.EXPLODE, 1, 1);
+        l.sendTitle(ChatColor.RED + "DEFEAT", ChatColor.RED + w.getName() + ChatColor.WHITE + selectedMessage + ChatColor.GREEN + l.getName());
+        l.sendMessage(ChatColor.RED + "You Lost the Match");
+    }
+
+    @EventHandler
+    public void onFightStart(DuelStartEvent event) {
+        Player p = event.getPlayer1();
+        Player o = event.getPlayer2();
+
+        p.sendMessage(ChatColor.GREEN + "The Fight Has Started");
+        o.sendMessage(ChatColor.GREEN + "The Fight Has Started");
+    }
+}
---
+++ src/main/java/lol/siwoo/drizzyPracticeCore/design/WarningMessage.java
@@ -0,0 +1,19 @@
+package lol.siwoo.drizzyPracticeCore.design;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import ga.strikepractice.events.KitSelectEvent;
+
+public class WarningMessage extends JavaPlugin implements Listener {
+
+    @EventHandler
+    public void onKitSelect(KitSelectEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage("");
+        player.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "WARNING! " + ChatColor.YELLOW + "Butterfly/Drag Clicking Is Not Recommended And is false-punishable.");
+        player.sendMessage("");
+    }
+}
