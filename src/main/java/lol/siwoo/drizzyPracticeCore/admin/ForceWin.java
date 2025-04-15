d2554221a40da43f7852f3e3afc44f5b4b1a81a9
siwoo <jamieyun06@gmail.com>
2025-04-15 10:56:20 +0900

    another fix

--- src/main/java/lol/siwoo/drizzyPracticeCore/admin/ForceWin.java
+++ src/main/java/lol/siwoo/drizzyPracticeCore/admin/ForceWin.java
@@ -2,6 +2,8 @@ package lol.siwoo.drizzyPracticeCore.admin;
 
 import ga.strikepractice.StrikePractice;
 import ga.strikepractice.api.StrikePracticeAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,29 +17,30 @@ public class ForceWin implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         Player p = (Player) sender;
 
-        p.sendMessage("test");
-
         if(p.hasPermission("strikecommands.admin")) {
             if (args.length == 0) {
                 api.forceWinFight(p);
-                p.sendMessage("Force Won The Fight!");
+                p.sendMessage(ChatColor.GREEN + "Force Won The Fight!");
             } else if(args.length == 1) {
                 // detect args type
-                if(args[1].equalsIgnoreCase("fight")) {
+                if(args[0].equalsIgnoreCase("fight")) {
+                    api.getFight(p).getPlayersInFight().forEach(player -> {
+                        player.teleport(api.getSpawnLocation());
+                    });
                     api.forceWinFight(p);
-                    p.sendMessage("Force Won The Fight!");
-                } else if(args[1].equalsIgnoreCase("round")) {
+                    p.sendMessage(ChatColor.GREEN + "Force Won The Fight!");
+                } else if(args[0].equalsIgnoreCase("round")) {
                     api.forceWinRound(p);
-                    p.sendMessage("Force Won The Round!");
+                    p.sendMessage(ChatColor.GREEN + "Force Won The Round!");
                 } else {
-                    p.sendMessage("Invalid Option! Only Use (fight/round)");
+                    p.sendMessage(ChatColor.RED + "Invalid Option! Only Use (fight/round)");
                 }
             } else {
-                p.sendMessage("Usage: /forcewin (fight/round)");
+                p.sendMessage(ChatColor.RED + "Usage: /forcewin (fight/round)");
             }
             return true;
         } else {
-            p.sendMessage("You don't have permission to use this command!");
+            p.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
             return true;
         }
     }
