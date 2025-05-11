b43129e57885bbe51c8b0cde0430eacea11e90c1
siwoo <jamieyun06@gmail.com>
2025-05-11 16:52:43 +0900

    more events for flight

--- src/main/java/lol/siwoo/acemcpracticecore/lobby/FlightListener.java
+++ src/main/java/lol/siwoo/acemcpracticecore/lobby/FlightListener.java
@@ -2,6 +2,8 @@ package lol.siwoo.acemcpracticecore.lobby;
 
 import ga.strikepractice.StrikePractice;
 import ga.strikepractice.api.StrikePracticeAPI;
+import ga.strikepractice.events.BotDuelEndEvent;
+import ga.strikepractice.events.DuelEndEvent;
 import ga.strikepractice.events.FightEndEvent;
 import ga.strikepractice.events.FightStartEvent;
 import org.bukkit.entity.Player;
@@ -30,6 +32,24 @@ public class FlightListener implements Listener {
 
     @EventHandler
     public void onFightEnd(FightEndEvent e) {
+        e.getFight().getPlayersInFight().forEach( player -> {
+            if (player.hasPermission("faramcpracticecore.fly")) {
+                player.setAllowFlight(true);
+            }
+        });
+    }
+
+    @EventHandler
+    public void onDuelEnd(DuelEndEvent e) {
+        e.getFight().getPlayersInFight().forEach( player -> {
+            if (player.hasPermission("faramcpracticecore.fly")) {
+                player.setAllowFlight(true);
+            }
+        });
+    }
+
+    @EventHandler
+    public void onBotDuelEnd(BotDuelEndEvent e) {
         e.getFight().getPlayersInFight().forEach( player -> {
             if (player.hasPermission("faramcpracticecore.fly")) {
                 player.setAllowFlight(true);
