44453e33894ba3e239c94266279ac10a597b93b4
siwoo <jamieyun06@gmail.com>
2025-05-25 20:07:03 +0900

    GetPlayerHead.java

---
+++ src/main/java/lol/siwoo/faramcpracticecore/util/GetPlayerHead.java
@@ -0,0 +1,29 @@
+package lol.siwoo.faramcpracticecore.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import java.util.UUID;
+
+public class GetPlayerHead {
+    public static ItemStack getPlayerHead(String playerName) {
+        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+        if (skullMeta != null) {
+            skullMeta.setOwner(playerName);
+            head.setItemMeta(skullMeta);
+        }
+        return head;
+    }
+
+    public static ItemStack getPlayerHead(UUID uuid) {
+        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+        if (skullMeta != null) {
+            skullMeta.setOwner(Bukkit.getOfflinePlayer(uuid).getName());
+            head.setItemMeta(skullMeta);
+        }
+        return head;
+    }
+}
