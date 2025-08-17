02b7e5390cbdb7298a4c6ce83108c80c0b89fc3d
siwoo <jamieyun06@gmail.com>
2025-08-17 16:21:20 +0900

    terms done #1

--- src/main/java/lol/siwoo/faramcpracticecore/FaraMCPracticeCore.java
+++ src/main/java/lol/siwoo/faramcpracticecore/FaraMCPracticeCore.java
@@ -4,9 +4,10 @@ import ga.strikepractice.StrikePractice;
 import ga.strikepractice.api.StrikePracticeAPI;
 import lol.siwoo.faramcpracticecore.aa.siwwa_pvp_1.DataLogger;
 import lol.siwoo.faramcpracticecore.aa.status.StatusChecker;
+import lol.siwoo.faramcpracticecore.aa.terms.Agree;
+import lol.siwoo.faramcpracticecore.aa.terms.Disagree;
 import lol.siwoo.faramcpracticecore.admin.*;
 import lol.siwoo.faramcpracticecore.aicoach.AICoach;
-import lol.siwoo.faramcpracticecore.aicoach.AICoachListener;
 import lol.siwoo.faramcpracticecore.design.FightEnd;
 import lol.siwoo.faramcpracticecore.design.WarningMessage;
 import lol.siwoo.faramcpracticecore.gamemode.BedFight;
@@ -18,6 +19,7 @@ import lol.siwoo.faramcpracticecore.party.HurryUpPartyOwner;
 import lol.siwoo.faramcpracticecore.party.SuggestPartyOwner;
 import lol.siwoo.faramcpracticecore.party.SuggestPartyOwnerListener;
 import lol.siwoo.faramcpracticecore.util.WebhookMessage;
+import net.md_5.bungee.api.chat.*;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -85,6 +87,29 @@ public final class FaraMCPracticeCore extends JavaPlugin implements Listener {
                 p.sendMessage(ChatColor.GREEN + "give siwoolol some money meh hes greedy");
             }
         }.runTaskLater(this, 80L);
+
+        p.sendMessage(
+                ChatColor.DARK_GRAY + "Beta Test and Data Analytics Agreement\n" +
+                        ChatColor.GRAY + "By accepting this agreement, you consent to provide FaraMC (operating under the online names \"siwoo,\" \"velocated,\" and \"worldy\" at faramc.uk) with access to collect gameplay data during our beta testing period.\n" +
+                        "1. Data Collection: You grant FaraMC permission to collect and analyze gameplay analytics and performance data (\"Data\") resulting from your use of the software.\n" +
+                        "2. Use of Data: FaraMC will use this Data for the purpose of product improvement, which includes, but is not limited to, enhancing the player experience, identifying and fixing bugs, and training specific data models.\n" +
+                        "3. Disclaimer of Warranty & Limitation of Liability: The beta software is provided on an \"as-is\" and \"as-available\" basis. By participating, you understand and agree that FaraMC shall not be liable for any direct, indirect, incidental, or consequential damages arising from your participation in the beta program or from the collection and use of Data as outlined in this agreement. You hereby waive any right to bring a claim or lawsuit against FaraMC for such damages.\n" +
+                        "By proceeding, you confirm that you have read, understood, and agree to be bound by all statements in this agreement.");
+        p.sendMessage(ChatColor.YELLOW + "Please read the agreement carefully before proceeding." + ChatColor.DARK_GRAY);
+
+        TextComponent agree = new TextComponent(ChatColor.BOLD.toString() + ChatColor.GREEN + "Agree | ");
+        agree.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{
+                new TextComponent(ChatColor.GRAY + "By Clicking This,\nYou confirm that you have read, understood, and agree\nto be bound by all statements in this agreement.")
+        }));
+        agree.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/terms+agree"));
+        TextComponent disagree = new TextComponent(ChatColor.RED + "Disagree");
+        disagree.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{
+                new TextComponent(ChatColor.RED + "You have to agree in to continue playing\n" + ChatColor.RED + "the beta version of the server.")
+        }));
+        disagree.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/terms+disagree"));
+
+        p.spigot().sendMessage(agree, disagree);
+        p.sendMessage("\n");
     }
 
     private void registerEvents() {
@@ -115,6 +140,8 @@ public final class FaraMCPracticeCore extends JavaPlugin implements Listener {
         getCommand("gmsp").setExecutor(new GMSP());
         getCommand("gma").setExecutor(new GMA());
         getCommand("sudo").setExecutor(new Sudo());
+        getCommand("terms+agree").setExecutor(new Agree());
+        getCommand("terms+disagree").setExecutor(new Disagree());
     }
 
     public void emergencyShutDown() {
