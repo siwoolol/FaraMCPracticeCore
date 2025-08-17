694a81db720a90f5909790f71d7adc8d094dbc78
siwoo <jamieyun06@gmail.com>
2025-08-17 17:27:28 +0900

    block commands if player didnt agree

--- src/main/java/lol/siwoo/faramcpracticecore/aa/terms/JoinMessage.java
+++ src/main/java/lol/siwoo/faramcpracticecore/aa/terms/JoinMessage.java
@@ -7,6 +7,8 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -77,5 +79,15 @@ public class JoinMessage {
                 p.sendMessage("\n");
             }
         }.runTaskLater(plugin, 25L);
+    }
+
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent e) {
+        Player p = e.getPlayer();
+
+        if (!(agreementUtils.hasPlayerAgreed(p))) {
+            e.setCancelled(true);
+            p.sendMessage(ChatColor.RED + "You must agree to the terms and conditions first before playing the beta version of the server.");
+        }
     }
 }
