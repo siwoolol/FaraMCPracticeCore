5af324778b11ab60dec65b5681def2adca428a12
siwoo <jamieyun06@gmail.com>
2025-09-13 20:28:54 +0900

    edit gui to adapt placeholder api

--- src/main/java/lol/siwoo/faramcpracticecore/design/UnrankedGUI.java
+++ src/main/java/lol/siwoo/faramcpracticecore/design/UnrankedGUI.java
@@ -40,48 +40,48 @@ public class UnrankedGUI implements CommandExecutor {
         // Add queue items
         addQueueItem(gui, 10, Material.DIAMOND_CHESTPLATE, "Boxing",
                 ChatColor.GREEN + "Boxing",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_boxing>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_boxing>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_boxing%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_boxing%");
         addQueueItem(gui, 11, createRegenerationPotion().getType(), "Nodebuff",
                 ChatColor.GOLD + "Nodebuff",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_nodebuff>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_nodebuff>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_nodebuff%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_nodebuff%");
         addQueueItem(gui, 12, Material.LAVA_BUCKET, "BuildUHC",
                 ChatColor.YELLOW + "BuildUHC",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_builduhc>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_builduhc>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_builduhc%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_builduhc%");
         addQueueItem(gui, 13, Material.LEASH, "Sumo",
                 ChatColor.LIGHT_PURPLE + "Sumo",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_sumo>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_sumo>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_sumo%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_sumo%");
         addQueueItem(gui, 14, Material.LEASH, "Sumo (Best of 3)",
                 ChatColor.RED + "Sumo (Best of 3)",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_sumobestof3>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_sumobestof3>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_sumobestof3%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_sumobestof3%");
         addQueueItem(gui, 15, Material.MUSHROOM_SOUP, "Soup",
                 ChatColor.AQUA + "Soup",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_soup>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_soup>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_soup%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_soup%");
         addQueueItem(gui, 16, Material.DIAMOND_AXE, "Axe",
                 ChatColor.DARK_RED + "Axe",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_axe>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_axe>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_axe%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_axe%");
         addQueueItem(gui, 19, Material.FISHING_ROD, "Combo",
                 ChatColor.DARK_GREEN + "Combo",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_combo>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_combo>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_combo%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_combo%");
         addQueueItem(gui, 20, Material.GOLDEN_APPLE, "Gapple",
                 ChatColor.DARK_GREEN + "Gapple",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_gapple>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_gapple>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_gapple%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_gapple%");
         addQueueItem(gui, 21, Material.BED, "BedFight",
                 ChatColor.DARK_GREEN + "BedFight",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_bedfight>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_bedfight>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_bedfight%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_bedfight%");
         addQueueItem(gui, 22, Material.FIREBALL, "Fireball Fight",
                 ChatColor.DARK_GREEN + "Fireball Fight",
-                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "<in_queue_count_fireballfight>",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "<in_fight_count_fireballfight>");
+                ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_fireballfight%",
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_fireballfight%");
 
         if (slot != 0) {
             ItemStack item = new ItemStack(Material.REDSTONE_BLOCK);
