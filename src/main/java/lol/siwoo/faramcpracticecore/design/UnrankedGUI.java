751b528fdbef9dd5815f46a696b5669222d1cd76
siwoo <jamieyun06@gmail.com>
2025-09-13 21:52:13 +0900

    replace placeholders directly

--- src/main/java/lol/siwoo/faramcpracticecore/design/UnrankedGUI.java
+++ src/main/java/lol/siwoo/faramcpracticecore/design/UnrankedGUI.java
@@ -1,5 +1,6 @@
 package lol.siwoo.faramcpracticecore.design;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -31,7 +32,7 @@ public class UnrankedGUI implements CommandExecutor {
         return true;
     }
 
-    public static Inventory createQueueGUI(Player player, int slot, String name) {
+    public static Inventory createQueueGUI(Player p, int slot, String name) {
         Inventory gui = Bukkit.createInventory(null, 45, ChatColor.YELLOW.toString() + ChatColor.BOLD + "Unranked Queue");
 
         // Fill background
@@ -41,47 +42,47 @@ public class UnrankedGUI implements CommandExecutor {
         addQueueItem(gui, 10, Material.DIAMOND_CHESTPLATE, "Boxing",
                 ChatColor.GREEN + "Boxing",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_boxing%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_boxing%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_boxing%", p);
         addQueueItem(gui, 11, createRegenerationPotion().getType(), "Nodebuff",
                 ChatColor.GOLD + "Nodebuff",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_nodebuff%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_nodebuff%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_nodebuff%", p);
         addQueueItem(gui, 12, Material.LAVA_BUCKET, "BuildUHC",
                 ChatColor.YELLOW + "BuildUHC",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_builduhc%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_builduhc%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_builduhc%", p);
         addQueueItem(gui, 13, Material.LEASH, "Sumo",
                 ChatColor.LIGHT_PURPLE + "Sumo",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_sumo%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_sumo%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_sumo%", p);
         addQueueItem(gui, 14, Material.LEASH, "Sumo (Best of 3)",
                 ChatColor.RED + "Sumo (Best of 3)",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_sumobestof3%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_sumobestof3%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_sumobestof3%", p);
         addQueueItem(gui, 15, Material.MUSHROOM_SOUP, "Soup",
                 ChatColor.AQUA + "Soup",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_soup%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_soup%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_soup%", p);
         addQueueItem(gui, 16, Material.DIAMOND_AXE, "Axe",
                 ChatColor.DARK_RED + "Axe",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_axe%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_axe%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_axe%", p);
         addQueueItem(gui, 19, Material.FISHING_ROD, "Combo",
                 ChatColor.DARK_GREEN + "Combo",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_combo%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_combo%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_combo%", p);
         addQueueItem(gui, 20, Material.GOLDEN_APPLE, "Gapple",
                 ChatColor.DARK_GREEN + "Gapple",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_gapple%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_gapple%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_gapple%", p);
         addQueueItem(gui, 21, Material.BED, "BedFight",
                 ChatColor.DARK_GREEN + "BedFight",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_bedfight%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_bedfight%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_bedfight%", p);
         addQueueItem(gui, 22, Material.FIREBALL, "Fireball Fight",
                 ChatColor.DARK_GREEN + "Fireball Fight",
                 ChatColor.GRAY + "Queued: "  + ChatColor.AQUA + "%strikepractice_in_queue_count_fireballfight%",
-                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_fireballfight%");
+                ChatColor.GRAY + "Playing: " + ChatColor.AQUA + "%strikepractice_in_fight_count_fireballfight%", p);
 
         if (slot != 0) {
             ItemStack item = new ItemStack(Material.REDSTONE_BLOCK);
@@ -132,11 +133,13 @@ public class UnrankedGUI implements CommandExecutor {
     }
 
     private static void addQueueItem(Inventory gui, int slot, Material material, String gameMode,
-                                     String displayName, String queued, String playing) {
+                                     String displayName, String queued, String playing, Player p) {
         ItemStack item = new ItemStack(material);
         ItemMeta meta = item.getItemMeta();
 
         meta.setDisplayName(displayName);
+        queued = PlaceholderAPI.setPlaceholders(p, queued);
+        playing = PlaceholderAPI.setPlaceholders(p, playing);
         meta.setLore(Arrays.asList(
                 queued,
                 playing,
