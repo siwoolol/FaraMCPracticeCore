0e3f5fb1465128b1ee56cad9d9170f4a80e58334
siwoo <jamieyun06@gmail.com>
2025-10-05 17:53:29 +0900

    add PreventServerStop listener to block server stop commands and notify players with an unknown command message

---
+++ src/main/java/lol/siwoo/faramcpracticecore/aa/aegis/preventServerStop.java
@@ -0,0 +1,22 @@
+package lol.siwoo.faramcpracticecore.aa.aegis;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class preventServerStop implements Listener {
+
+    @EventHandler
+    public void onCommandExecute(PlayerCommandPreprocessEvent e) {
+        Player p = e.getPlayer();
+
+        if (e.getMessage().equalsIgnoreCase("/stop")
+                || e.getMessage().equalsIgnoreCase("/bukkit:stop")
+                || e.getMessage().equalsIgnoreCase("/minecraft:stop")) {
+            e.setCancelled(true);
+            p.sendMessage(ChatColor.GRAY + "Unknown command. Type " + ChatColor.RED + " /help " + ChatColor.GRAY + "for help.");
+        }
+    }
+}
