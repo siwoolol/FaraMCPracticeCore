fe39628162e6b21071b9347cd9c244ca40d6afec
siwoo <jamieyun06@gmail.com>
2025-10-18 19:28:23 +0900

    adjust splash potion velocity in PotThrowMech for flatter trajectory

--- src/main/java/lol/siwoo/faramcpracticecore/fix/PotThrowMech.java
+++ src/main/java/lol/siwoo/faramcpracticecore/fix/PotThrowMech.java
@@ -7,18 +7,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 
+/**
+ * Modifies the velocity of thrown splash potions to mimic the mechanics of servers like Minemen.club.
+ * This results in a faster and flatter potion trajectory.
+ */
 public class PotThrowMech implements Listener {
 
     @EventHandler
     public void onProjectileLaunch(ProjectileLaunchEvent event) {
-        if (event.getEntityType() != EntityType.SPLASH_POTION
-                || !(event.getEntity().getShooter() instanceof Player)) {
+        if (event.getEntityType() != EntityType.SPLASH_POTION) {
+            return;
+        }
+
+        if (!(event.getEntity().getShooter() instanceof Player)) {
             return;
         }
 
         Player p = (Player) event.getEntity().getShooter();
         ThrownPotion potion = (ThrownPotion) event.getEntity();
 
-        potion.setVelocity(p.getEyeLocation().getDirection().multiply(1.05D));
+        potion.setVelocity(p.getEyeLocation().getDirection().multiply(0.67));
     }
 }
