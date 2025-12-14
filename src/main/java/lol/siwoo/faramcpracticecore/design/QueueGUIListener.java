5fe0df1c119ed463ba71d967209201a08dd4eda5
siwoo <jamieyun06@gmail.com>
2025-12-14 15:42:26 +0900

    better queue logic through api

--- src/main/java/lol/siwoo/faramcpracticecore/design/QueueGUIListener.java
+++ src/main/java/lol/siwoo/faramcpracticecore/design/QueueGUIListener.java
@@ -1,9 +1,13 @@
 package lol.siwoo.faramcpracticecore.design;
 
+import ga.strikepractice.StrikePractice;
+import ga.strikepractice.api.StrikePracticeAPI;
+import ga.strikepractice.battlekit.BattleKit;
 import ga.strikepractice.events.DuelStartEvent;
 import ga.strikepractice.events.FightStartEvent;
 import ga.strikepractice.events.KitSelectEvent;
 import lol.siwoo.faramcpracticecore.FaraMCPracticeCore;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -11,6 +15,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -19,6 +24,7 @@ import javax.xml.stream.events.StartDocument;
 public class QueueGUIListener implements Listener {
 
     private final FaraMCPracticeCore plugin;
+    StrikePracticeAPI api = StrikePractice.getAPI();
 
     public QueueGUIListener(FaraMCPracticeCore plugin) {
         this.plugin = plugin;
@@ -42,40 +48,40 @@ public class QueueGUIListener implements Listener {
 
         // Execute queue commands based on clicked item
         if (clickedItem.getType().equals(Material.REDSTONE_BLOCK)) {
-            player.performCommand("queue leave");
+            Bukkit.dispatchCommand(player, "queue leave");
             newafterActivities(event);
         } else if (itemName.contains("Boxing")) {
-            player.performCommand("queue boxing");
+            api.joinQueue(player, BattleKit.getKit("boxing"));
             afterActivities(event);
         } else if (itemName.contains("Nodebuff")) {
-            player.performCommand("queue nodebuff");
+            api.joinQueue(player, BattleKit.getKit("nodebuff"));
             afterActivities(event);
         } else if (itemName.contains("BuildUHC")) {
-            player.performCommand("queue builduhc");
+            api.joinQueue(player, BattleKit.getKit("builduhc"));
             afterActivities(event);
         } else if (itemName.contains("Sumo (Best of 3)")) {
-            player.performCommand("queue sumobestof3");
+            api.joinQueue(player, BattleKit.getKit("sumobestof3"));
             afterActivities(event);
         } else if (itemName.contains("Sumo")) {
-            player.performCommand("queue sumo");
+            api.joinQueue(player, BattleKit.getKit("sumo"));
             afterActivities(event);
         } else if (itemName.contains("Soup")) {
-            player.performCommand("queue soup");
+            api.joinQueue(player, BattleKit.getKit("soup"));
             afterActivities(event);
         } else if (itemName.contains("Axe")) {
-            player.performCommand("queue axepvp");
+            api.joinQueue(player, BattleKit.getKit("axepvp"));
             afterActivities(event);
         } else if (itemName.contains("Combo")) {
-            player.performCommand("queue combo");
+            api.joinQueue(player, BattleKit.getKit("combo"));
             afterActivities(event);
         } else if (itemName.contains("Gapple")) {
-            player.performCommand("queue gapple");
+            api.joinQueue(player, BattleKit.getKit("gapple"));
             afterActivities(event);
         } else if (itemName.contains("BedFight")) {
-            player.performCommand("queue bedfight");
+            api.joinQueue(player, BattleKit.getKit("bedfight"));
             afterActivities(event);
         } else if (itemName.contains("Fireball Fight")) {
-            player.performCommand("queue fireballfight");
+            api.joinQueue(player, BattleKit.getKit("fireballfight"));
             afterActivities(event);
         }
     }
@@ -84,12 +90,11 @@ public class QueueGUIListener implements Listener {
         Player p = (Player) e.getWhoClicked();
         p.playSound(p.getLocation(), Sound.WOOD_CLICK, 1, 1);
         p.openInventory(UnrankedGUI.createQueueGUI(p, 0, "n word"));
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                p.openInventory(UnrankedGUI.createQueueGUI(p, e.getSlot(), e.getCurrentItem().getItemMeta().getDisplayName()));
-            }
-        }.runTaskLater(plugin, 0L);
+        p.openInventory(UnrankedGUI.createQueueGUI(p, e.getSlot(), e.getCurrentItem().getItemMeta().getDisplayName()));
+    }
+
+    public void updateInventory(Inventory i) {
+        i.getSize();
     }
 
     public void newafterActivities(InventoryClickEvent e) {
