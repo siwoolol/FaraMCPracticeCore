78a45b36bf9cab1ab786f9fdd68f4247e3f2345a
siwoo <99875626+siwoolol@users.noreply.github.com>
2025-12-28 15:43:53 +0900

    make basic stuff for airforce gamemode

--- src/main/java/lol/siwoo/faramcpracticecore/gamemode/AirForce.java
+++ src/main/java/lol/siwoo/faramcpracticecore/gamemode/AirForce.java
@@ -1,1 +1,64 @@
-public void 
+package lol.siwoo.faramcpracticecore.gamemode;
+
+import org.bukkit.event.Listener;
+
+public class AirForce implements Listener {
+
+    private int fightCounter = 0;
+
+    @EventHandler
+    public void onFightStart(FightStartEvent e) {
+        if (!e.getFight().getKit().getName().equalsIgnoreCase("airforce")) {
+            return;
+        }
+
+        String fightId = "airforce_" + (++fightCounter) + "_" + System.currentTimeMillis();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                e.getFight().getPlayersInFight().forEach(p --> {
+                    UUID playerId = p.getUniqueId();
+                    fightIds.put(playerId.toString(), fightId);
+
+                    summonAirCrafts(p);
+                });
+            }
+        }.runTaskLater(plugin, 2L);
+    }
+
+    @EventHandler
+    public void onFightEnd(FightEndEvent e) {
+        if (!e.getFight().getKit().getName().equalsIgnoreCase("airforce")) {
+            return;
+        }
+
+        String fightId = null;
+        for (Player p : e.getFight().getPlayersinFight()) {
+            fightId = fightIds.get(p.uniqueId().toString());
+            if (fightId != null) {
+                break;
+            }
+        }
+
+        if (fightId != null) {
+            final String finalFightId = fightId;
+            plugin.getLogger().info("Fight ended with ID: " + fightId + ".");
+        }
+
+        e.getFight().getPlayersInFight().forEach(p -> {
+            UUID playerId = p.getUniqueId();
+            fightIds.remove(playerId.toString());
+        });
+    }
+
+    public void summonAirCrafts(Player p) {
+        // TODO make this shit
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        UUID playerId = e.getPlayer().getUniqueId();
+        fightIds.remove(playerId.toString());
+    }
+}
