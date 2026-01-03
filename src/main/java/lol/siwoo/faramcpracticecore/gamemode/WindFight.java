2d106028104d21dd49353ef977e3c0c065ee854b
siwoo <jamieyun06@gmail.com>
2026-01-03 22:53:02 +0900

    cooldown for skills

--- src/main/java/lol/siwoo/faramcpracticecore/gamemode/WindFight.java
+++ src/main/java/lol/siwoo/faramcpracticecore/gamemode/WindFight.java
@@ -17,8 +17,11 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
 import java.util.UUID;
 
 public class WindFight implements Listener {
@@ -26,6 +29,8 @@ public class WindFight implements Listener {
     private final FaraMCPracticeCore plugin;
     private final StrikePracticeAPI api = StrikePractice.getAPI();
     private final Map<String, String> fightIds;
+    private final Map<String, Integer> lastLaunch = new HashMap<>();
+    private final Map<String, Integer> lastPush = new HashMap<>();
     private int fightCounter = 0;
 
     public WindFight(FaraMCPracticeCore plugin) {
@@ -75,6 +80,8 @@ public class WindFight implements Listener {
         e.getFight().getPlayersInFight().forEach(p -> {
             UUID playerId = p.getUniqueId();
             fightIds.remove(playerId.toString());
+            lastLaunch.remove(playerId.toString());
+            lastPush.remove(playerId.toString());
         });
     }
 
@@ -92,8 +99,18 @@ public class WindFight implements Listener {
         }
 
         if (itemName.contains("Launch ")) {
+            if (lastLaunch.get(p.getUniqueId().toString()) + 1000 >= System.currentTimeMillis()) {
+                return;
+            }
+
+            lastLaunch.put(p.getUniqueId().toString(), (int) System.currentTimeMillis());
             launchPlayer(p);
         } else if (itemName.contains("Push ")) {
+            if (lastPush.get(p.getUniqueId().toString()) + 1000 >= System.currentTimeMillis()) {
+                return;
+            }
+
+            lastPush.put(p.getUniqueId().toString(), (int) System.currentTimeMillis());
             pushEntity(p);
         }
     }
@@ -131,5 +148,7 @@ public class WindFight implements Listener {
     public void onPlayerQuit(PlayerQuitEvent e) {
         UUID playerId = e.getPlayer().getUniqueId();
         fightIds.remove(playerId.toString());
+        lastLaunch.remove(playerId.toString());
+        lastPush.remove(playerId.toString());
     }
 }
