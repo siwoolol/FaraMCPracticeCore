f3f3d20f94a4b231945fde668eb6a12878bf35c7
siwoo <jamieyun06@gmail.com>
2026-01-21 14:25:24 +0900

    RBWFFA #1

--- src/main/java/lol/siwoo/faramcpracticecore/FaraMCPracticeCore.java
+++ src/main/java/lol/siwoo/faramcpracticecore/FaraMCPracticeCore.java
@@ -17,6 +17,7 @@ import lol.siwoo.faramcpracticecore.fix.PotThrowMech;
 import lol.siwoo.faramcpracticecore.gamemode.BedFight;
 import lol.siwoo.faramcpracticecore.gamemode.Boxing;
 //import lol.siwoo.faramcpracticecore.gamemode.FireballFight;
+import lol.siwoo.faramcpracticecore.gamemode.RBWFFA;
 import lol.siwoo.faramcpracticecore.gamemode.WindFight;
 import lol.siwoo.faramcpracticecore.lobby.Flight;
 import lol.siwoo.faramcpracticecore.lobby.FlightListener;
@@ -142,6 +143,7 @@ public final class FaraMCPracticeCore extends JavaPlugin implements Listener {
         pm.registerEvents(new BedFight(this), this);
 //        pm.registerEvents(new FireballFight(this), this);
         pm.registerEvents(new WindFight(this), this);
+        pm.registerEvents(new RBWFFA(this), this);
 //        pm.registerEvents(new AICoachListener(aiCoach, strikePracticeAPI), this);
 
 //        DataLogger dataLogger = new DataLogger(this);
---
+++ src/main/java/lol/siwoo/faramcpracticecore/gamemode/RBWFFA.java
@@ -0,0 +1,66 @@
+package lol.siwoo.faramcpracticecore.gamemode;
+
+import ga.strikepractice.StrikePractice;
+import ga.strikepractice.api.StrikePracticeAPI;
+import lol.siwoo.faramcpracticecore.FaraMCPracticeCore;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class RBWFFA implements Listener {
+    private final FaraMCPracticeCore plugin;
+    private final StrikePracticeAPI api;
+
+    public RBWFFA(FaraMCPracticeCore plugin) {
+        this.plugin = plugin;
+        this.api = StrikePractice.getAPI();
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        if (api.getFight(p).getArena().equals("rbwffa")) {
+            if (p.getLocation().getY() < api.getFight(p).getArena().getLoc1().getY() - 60) {
+                p.damage(69420.0);
+                for (Player player : p.getWorld().getPlayers()) {
+                    player.sendMessage(Component.text(p.getName() + " died").color(NamedTextColor.GRAY));
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        if (!(e.getEntity() instanceof Player)) return;
+        Player p = (Player) e.getEntity();
+        if (api.getFight(p).getArena().equals("rbwffa")) {
+            if (p.getHealth() - e.getFinalDamage() <= 1f) {
+                for (Player player : p.getWorld().getPlayers()) {
+                    player.sendMessage(Component.text(p.getName() + " died").color(NamedTextColor.GRAY));
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Player p = e.getPlayer();
+        ItemStack block = (ItemStack) e.getBlockPlaced();
+        if (api.getFight(p).getArena().equals("rbwffa")) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    p.give(block);
+                }
+            }.runTaskLater(plugin, 100L);
+        }
+    }
+}
